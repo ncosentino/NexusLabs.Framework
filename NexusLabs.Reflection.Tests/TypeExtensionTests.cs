4063c337a43bd1c56b5a339a2112df89b3f14956
Nick Cosentino <n.b.cosentino@gmail.com>
2020-08-23 14:48:31 -0400

    Type Extensions for Creating Instances

---
+++ NexusLabs.Reflection.Tests/TypeExtensionTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace NexusLabs.Reflection.Tests
+{
+    public sealed class TypeExtensionTests
+    {
+        [Fact]
+        private void CreateInstance_PrivateConstructorNoParameters_CreatesInstance()
+        {
+            var result = typeof(PrivateConstructorType).CreateInstance(c => true);
+            Assert.NotNull(result);
+            Assert.IsType<PrivateConstructorType>(result);
+        }
+
+        [Fact]
+        private void CreateInstance_PrivateConstructorStringParameter_CreatesInstanceWithParameter()
+        {
+            var result = typeof(PrivateConstructorType).CreateInstance(c =>
+            {
+                var parameters = c.GetParameters();
+                return parameters.Length == 1 && parameters[0].ParameterType == typeof(string);
+            },
+            new object[] { "expected value" });
+
+            Assert.NotNull(result);
+            Assert.IsType<PrivateConstructorType>(result);
+            Assert.Equal("expected value", result.GetField("_stringParameter"));
+        }
+
+        [Fact]
+        private void CreateInstance_PrivateConstructorParameterTypeMismatch_ThrowsArgumentException()
+        {
+            Action method = () => typeof(PrivateConstructorType).CreateInstance(c =>
+            {
+                var parameters = c.GetParameters();
+                return parameters.Length == 1 && parameters[0].ParameterType == typeof(string);
+            },
+            new object[] { 123 });
+
+            Assert.Throws<ArgumentException>(method);
+        }
+
+        private sealed class PrivateConstructorType
+        {
+            private readonly string _stringParameter;
+
+            private PrivateConstructorType()
+            {
+            }
+
+            private PrivateConstructorType(string stringParameter)
+            {
+                _stringParameter = stringParameter;
+            }
+        }
+    }
+}
---
+++ NexusLabs.Reflection/TypeExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace NexusLabs.Reflection
+{
+    public static class TypeExtensions
+    {
+        public static object CreateInstance(
+            this Type type,
+            Predicate<ConstructorInfo> constructorFilter,
+            object[] parameters = null)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(
+                    nameof(type),
+                    "The type cannot be null.");
+            }
+
+            if (constructorFilter == null)
+            {
+                throw new ArgumentNullException(
+                    nameof(constructorFilter),
+                    "The constructor filter cannot be null.");
+            }
+
+            var constructors = type.GetConstructors(
+                BindingFlags.NonPublic |
+                BindingFlags.Public |
+                BindingFlags.Instance);
+            var constructor = constructors.FirstOrDefault(c => constructorFilter.Invoke(c));
+            if (constructor == null)
+            {
+                throw new InvalidOperationException(
+                    $"No valid constructor available to create instance of '{type}'.");
+            }
+
+            var instance = constructor.Invoke(parameters ?? new object[]
+            {
+            });
+            return instance;
+        }
+    }
+}

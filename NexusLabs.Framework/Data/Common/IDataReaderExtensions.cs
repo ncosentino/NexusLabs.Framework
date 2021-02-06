fe297899ba65a01e78a85e5cd5260d96365ad6ac
Nick Cosentino <n.b.cosentino@gmail.com>
2021-02-06 06:58:35 -0800

    More Data Reader Extensions

---
+++ NexusLabs.Framework/Data/Common/IDataReaderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace NexusLabs.Framework.Data.Common
+{
+    public static class IDataReaderExtensions
+    {
+        public static string GetNullableString(
+            this IDataReader reader,
+            int ordinal,
+            Func<string> nullValueCallback)
+        {
+            var result = reader.IsDBNull(ordinal)
+                ? (nullValueCallback == null ? null : nullValueCallback.Invoke())
+                : reader.GetString(ordinal);
+            return result;
+        }
+
+        public static string GetNullableString(
+            this IDataReader reader,
+            string name,
+            Func<string> nullValueCallback) =>
+            GetNullableString(reader, reader.GetOrdinal(name), nullValueCallback);
+
+        public static string GetNullableString(
+            this IDataReader reader,
+            int ordinal) =>
+            GetNullableString(reader, ordinal, null);
+
+        public static string GetNullableString(
+            this IDataReader reader,
+            string name) =>
+            GetNullableString(reader, name, null);
+    }
+}

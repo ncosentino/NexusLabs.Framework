37ffcbe8026229ec57947a8c26e95e885c730c79
Nick Cosentino <n.b.cosentino@gmail.com>
2021-02-06 06:56:51 -0800

    ToStream() String Extension
    - Convert a string into a stream ready to be consumed.

---
+++ NexusLabs.Framework/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static Stream ToStream(this string str, Encoding encoding)
+        {
+            var stream = new MemoryStream(encoding.GetBytes(str));
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}

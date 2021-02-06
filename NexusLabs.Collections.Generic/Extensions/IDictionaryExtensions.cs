1ec32ce75c3bd7efff8aa6970eb5535d2ef7d9ac
Nick Cosentino <n.b.cosentino@gmail.com>
2021-02-06 06:59:06 -0800

    Dictionary Extension to AddRange

---
+++ NexusLabs.Collections.Generic/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static class IDictionaryExtensions
+    {
+        public static void AddRange<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            IEnumerable<KeyValuePair<TKey, TValue>> items)
+        {
+            foreach (var kvp in items)
+            {
+                dictionary.Add(kvp);
+            }
+        }
+    }
+}

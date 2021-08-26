04f26edb7ba04a5fa2b9af0352fdcad125e6797c
Nick Cosentino <n.b.cosentino@gmail.com>
2021-08-26 11:56:31 -0700

    Reflection up Interface Hierarchy
    - Taken from:
      https://stackoverflow.com/a/2444090/2704424

--- NexusLabs.Reflection/TypeExtensions.cs
+++ NexusLabs.Reflection/TypeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -42,6 +43,96 @@ namespace NexusLabs.Reflection
                 constructorFilter,
                 parameters);
             return (T)instance;
+        }
+
+        public static IEnumerable<PropertyInfo> GetPublicProperties(this Type type)
+        {
+            if (type.IsInterface)
+            {
+                var considered = new List<Type>();
+                var queue = new Queue<Type>();
+                considered.Add(type);
+                queue.Enqueue(type);
+
+                while (queue.Count > 0)
+                {
+                    var subType = queue.Dequeue();
+                    foreach (var subInterface in subType.GetInterfaces())
+                    {
+                        if (considered.Contains(subInterface))
+                        {
+                            continue;
+                        }
+
+                        considered.Add(subInterface);
+                        queue.Enqueue(subInterface);
+                    }
+
+                    var typeMembers = subType.GetProperties(
+                        BindingFlags.FlattenHierarchy |
+                        BindingFlags.Public | 
+                        BindingFlags.Instance);
+
+                    foreach (var member in typeMembers)
+                    {
+                        yield return member;
+                    }
+                }
+            }
+
+            foreach (var member in type.GetProperties(
+                BindingFlags.FlattenHierarchy |
+                BindingFlags.Public | 
+                BindingFlags.Instance)
+            )
+            {
+                yield return member;
+            }
+        }
+
+        public static IEnumerable<MethodInfo> GetPublicMethods(this Type type)
+        {
+            if (type.IsInterface)
+            {
+                var considered = new List<Type>();
+                var queue = new Queue<Type>();
+                considered.Add(type);
+                queue.Enqueue(type);
+
+                while (queue.Count > 0)
+                {
+                    var subType = queue.Dequeue();
+                    foreach (var subInterface in subType.GetInterfaces())
+                    {
+                        if (considered.Contains(subInterface))
+                        {
+                            continue;
+                        }
+
+                        considered.Add(subInterface);
+                        queue.Enqueue(subInterface);
+                    }
+
+                    var typeMembers = subType.GetMethods(
+                        BindingFlags.FlattenHierarchy |
+                        BindingFlags.Public |
+                        BindingFlags.Instance);
+
+                    foreach (var member in typeMembers)
+                    {
+                        yield return member;
+                    }
+                }
+            }
+
+            foreach (var member in type.GetMethods(
+                BindingFlags.FlattenHierarchy |
+                BindingFlags.Public |
+                BindingFlags.Instance)
+            )
+            {
+                yield return member;
+            }
         }
     }
 }

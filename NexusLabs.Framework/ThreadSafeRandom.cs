532546b8005a9c2091a4a0169c97315a091687cc
Nick Cosentino <n.b.cosentino@gmail.com>
2022-02-01 17:50:17 -0800

    Thread Safe Random
    - Built in one is not thread safe.

---
+++ NexusLabs.Framework/ThreadSafeRandom.cs
@@ -0,0 +1,69 @@
+﻿namespace NexusLabs.Framework
+{
+    public sealed class ThreadSafeRandom : IRandom
+    {
+        private readonly object _lock;
+        private readonly IRandom _random;
+
+        public ThreadSafeRandom(IRandom random)
+        {
+            _lock = new object();
+            _random = random;
+        }
+
+        public double NextDouble(
+            double minInclusive,
+            double maxExclusive)
+        {
+            lock (_lock)
+            {
+                return _random.NextDouble(minInclusive, maxExclusive);
+            }
+        }
+
+        public double NextDouble(
+            double minInclusive, 
+            double maxExclusive, 
+            double step)
+        {
+            lock (_lock)
+            {
+                return _random.NextDouble(minInclusive, maxExclusive, step);
+            }
+        }
+
+        public int Next(int minInclusive, int maxExclusive)
+        {
+            lock (_lock)
+            {
+                return _random.Next(minInclusive, maxExclusive);
+            }
+        }
+
+        public int Next(int minInclusive, int maxExclusive, int step)
+        {
+            lock (_lock)
+            {
+                return _random.Next(minInclusive, maxExclusive, step);
+            }
+        }
+
+        public long NextLong(
+            long minInclusive,
+            long maxExclusive)
+        {
+            lock (_lock)
+            {
+                return _random.NextLong(minInclusive, maxExclusive);
+            }
+        }
+
+        public long NextLong(long minInclusive, long maxExclusive, long step)
+        {
+            lock (_lock)
+            {
+                return _random.NextLong(minInclusive, maxExclusive, step);
+            }
+        }
+    }
+}

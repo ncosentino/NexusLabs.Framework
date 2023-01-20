9de4c084fac4aa7798623ce8590fb426acc368f7
Nick Cosentino <n.b.cosentino@gmail.com>
2023-01-20 12:40:48 -0800

    Random Tests

---
+++ NexusLabs.Framework.Tests/RandomTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace NexusLabs.Framework.Tests
+{
+    public sealed class RandomTests
+    {
+        private readonly Random _random;
+
+        public RandomTests()
+        {
+            _random = new Random(new System.Random(1137));
+        }
+
+        [InlineData(0, 1, 2, new double[] { 0, 2 })]
+        [InlineData(1, 1, 2, new double[] { 1 })]
+        [InlineData(1, 1, 1, new double[] { 1 })]
+        [InlineData(0, 1, 1, new double[] { 0, 1 })]
+        [InlineData(0, 0, 1, new double[] { 0 })]
+        [Theory]
+        private void NextDouble_ValidScenarios(
+            double min,
+            double max,
+            double step,
+            double[] possibleExpectedValues)
+        {
+            var result = _random.NextDouble(min, max, step);
+            Assert.Contains(
+                result,
+                possibleExpectedValues);
+        }
+    }
+}

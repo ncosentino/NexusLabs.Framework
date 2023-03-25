0b3a8c90381f407b4a11a297c3d20997360f12c1
Nick Cosentino <n.b.cosentino@gmail.com>
2023-03-25 16:20:17 -0700

    Extensions for Enumerables to AsyncEnumerables

---
+++ NexusLabs.Framework.Tests/TaskExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace NexusLabs.Framework.Tests
+{
+    public sealed class TaskExtensionsTests
+    {
+        [Fact]
+        private async Task ToUnorderedAsyncEnumerable_EarlyEnumerationExit_CancelsRemainingTasks()
+        {
+            const int TARGET_TAKE = 3;
+            var source = new int[] { 5, 4, 3, 2, 1 };
+            var results = await source
+                .ToUnorderedAsyncEnumerable(async (x, cancellationToken) =>
+                {
+                    if (x <= TARGET_TAKE)
+                    {
+                        return x;
+                    }
+
+                    await Task.Delay(x * 10000, cancellationToken);
+                    throw new InvalidOperationException(
+                        $"Task should have been cancelled before ever reaching this.");
+                })
+                .Take(TARGET_TAKE)
+                .ToArrayAsync();
+            Assert.Equal(
+                source.Where(x => x <= TARGET_TAKE).Take(TARGET_TAKE),
+                results);
+        }
+
+        [Fact]
+        private async Task ToOrderedAsyncEnumerable_EarlyEnumerationExit_CancelsRemainingTasks()
+        {
+            const int TARGET_TAKE = 3;
+            var source = new int[] { 1, 2, 3, 4, 5 };
+            var results = await source
+                .ToOrderedAsyncEnumerable(async (x, cancellationToken) =>
+                {
+                    if (x <= TARGET_TAKE)
+                    {
+                        return x;
+                    }
+
+                    await Task.Delay(x * 10000, cancellationToken);
+                    throw new InvalidOperationException(
+                        $"Task should have been cancelled before ever reaching this.");
+                })
+                .Take(TARGET_TAKE)
+                .ToArrayAsync();
+            Assert.Equal(
+                source.Where(x => x <= TARGET_TAKE).Take(TARGET_TAKE),
+                results);
+        }
+    }
+}
--- NexusLabs.Framework/Threading/Tasks/TaskExtensions.cs
+++ NexusLabs.Framework/Threading/Tasks/TaskExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace System.Threading.Tasks
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace System.Threading.Tasks
 {
     public static class TaskExtensions
     {
@@ -7,5 +11,71 @@
             // do nothing with this guy, but tells callers that we explicitly 
             // don't care about what happens
         }
+
+#if NET6_0_OR_GREATER
+        public static async IAsyncEnumerable<TResult> ToUnorderedAsyncEnumerable<TSource, TResult>(
+            this IEnumerable<TSource> items,
+            Func<TSource, CancellationToken, Task<TResult>> createTask,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            using var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+            var tasks = items.Select(x => createTask(x, cancellationTokenSource.Token));
+            var remainingTasks = tasks.ToHashSet();
+            try
+            {
+                while (remainingTasks.Count > 0)
+                {
+                    var completedTask = await Task
+                        .WhenAny(remainingTasks)
+                        .ConfigureAwait(false);
+                    remainingTasks.Remove(completedTask);
+                    var nextResult = completedTask.Result;
+                    yield return nextResult;
+                }
+            }
+            finally
+            {
+                cancellationTokenSource.Cancel();
+
+                try
+                {
+                    await Task.WhenAll(remainingTasks).ConfigureAwait(false);
+                }
+                catch (OperationCanceledException) { }
+            }
+        }
+
+        public static async IAsyncEnumerable<TResult> ToOrderedAsyncEnumerable<TSource, TResult>(
+            this IEnumerable<TSource> items,
+            Func<TSource, CancellationToken, Task<TResult>> createTask,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            using var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+            var tasks = items.Select(x => createTask(x, cancellationTokenSource.Token));
+            var remainingTasks = new Queue<Task<TResult>>(tasks);
+            try
+            {
+                while (remainingTasks.Count > 0)
+                {
+                    var nextTask = remainingTasks.Dequeue();
+                    await nextTask.ConfigureAwait(false);
+                    var nextResult = nextTask.Result;
+                    yield return nextResult;
+                }
+            }
+            finally
+            {
+                cancellationTokenSource.Cancel();
+
+                try
+                {
+                    await Task.WhenAll(remainingTasks).ConfigureAwait(false);
+                }
+                catch (OperationCanceledException) { }
+            }
+        }
+#endif
     }
 }

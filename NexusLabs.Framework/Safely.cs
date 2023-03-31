970af869c058174f7c89be0f72c6001826150181
Nick Cosentino <n.b.cosentino@gmail.com>
2023-03-30 22:55:47 -0700

    Safely Gets Error Callback

--- NexusLabs.Framework/Safely.cs
+++ NexusLabs.Framework/Safely.cs
@@ -5,63 +5,9 @@ namespace NexusLabs.Framework
 {
     public static class Safely
     {
-        public static Tried<T> GetResultOrFalse<T>(Func<T> callback)
-        {
-            try
-            {
-                var result = callback();
-                return result;
-            }
-            catch
-            {
-                return Tried<T>.Failed;
-            }
-        }
-
-        public static Tried<T> GetResultOrFalse<T>(Func<Tried<T>> callback)
-        {
-            try
-            {
-                var result = callback();
-                return result;
-            }
-            catch
-            {
-                return Tried<T>.Failed;
-            }
-        }
-
-        public static async Task<Tried<T>> GetResultOrFalseAsync<T>(Func<Task<T>> callback)
-        {
-            try
-            {
-                var result = await
-                    callback()
-                    .ConfigureAwait(false);
-                return result;
-            }
-            catch
-            {
-                return Tried<T>.Failed;
-            }
-        }
-
-        public static async Task<Tried<T>> GetResultOrFalseAsync<T>(Func<Task<Tried<T>>> callback)
-        {
-            try
-            {
-                var result = await
-                    callback()
-                    .ConfigureAwait(false);
-                return result;
-            }
-            catch
-            {
-                return Tried<T>.Failed;
-            }
-        }
-
-        public static TriedEx<T> GetResultOrException<T>(Func<T> callback)
+        public static Tried<T> GetResultOrFalse<T>(
+            Func<T> callback,
+            Action<Exception>? errorCallback = null)
         {
             try
             {
@@ -70,11 +16,94 @@ namespace NexusLabs.Framework
             }
             catch (Exception ex)
             {
+                errorCallback?.Invoke(ex);
+                return Tried<T>.Failed;
+            }
+        }
+
+        public static Tried<T> GetResultOrFalse<T>(
+            Func<Tried<T>> callback,
+            Action<Exception>? errorCallback = null)
+        {
+            try
+            {
+                var result = callback();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                errorCallback?.Invoke(ex);
+                return Tried<T>.Failed;
+            }
+        }
+
+        public static async Task<Tried<T>> GetResultOrFalseAsync<T>(
+            Func<Task<T>> callback,
+            Func<Exception, Task>? errorCallback = null)
+        {
+            try
+            {
+                var result = await
+                    callback()
+                    .ConfigureAwait(false);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                if (errorCallback != null)
+                {
+                    await errorCallback
+                        .Invoke(ex)
+                        .ConfigureAwait(false);
+                }
+
+                return Tried<T>.Failed;
+            }
+        }
+
+        public static async Task<Tried<T>> GetResultOrFalseAsync<T>(
+            Func<Task<Tried<T>>> callback,
+            Func<Exception, Task>? errorCallback = null)
+        {
+            try
+            {
+                var result = await
+                    callback()
+                    .ConfigureAwait(false);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                if (errorCallback != null)
+                {
+                    await errorCallback
+                        .Invoke(ex)
+                        .ConfigureAwait(false);
+                }
+
+                return Tried<T>.Failed;
+            }
+        }
+
+        public static TriedEx<T> GetResultOrException<T>(
+            Func<T> callback,
+            Action<Exception>? errorCallback = null)
+        {
+            try
+            {
+                var result = callback();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                errorCallback?.Invoke(ex);
                 return ex;
             }
         }
 
-        public static TriedEx<T> GetResultOrException<T>(Func<TriedEx<T>> callback)
+        public static TriedEx<T> GetResultOrException<T>(
+            Func<TriedEx<T>> callback,
+            Action<Exception>? errorCallback = null)
         {
             try
             {
@@ -83,11 +112,14 @@ namespace NexusLabs.Framework
             }
             catch (Exception ex)
             {
+                errorCallback?.Invoke(ex);
                 return ex;
             }
         }
 
-        public static async Task<TriedEx<T>> GetResultOrExceptionAsync<T>(Func<Task<T>> callback)
+        public static async Task<TriedEx<T>> GetResultOrExceptionAsync<T>(
+            Func<Task<T>> callback,
+            Func<Exception, Task>? errorCallback = null)
         {
             try
             {
@@ -98,11 +130,20 @@ namespace NexusLabs.Framework
             }
             catch (Exception ex)
             {
+                if (errorCallback != null)
+                {
+                    await errorCallback
+                        .Invoke(ex)
+                        .ConfigureAwait(false);
+                }
+
                 return ex;
             }
         }
 
-        public static async Task<TriedEx<T>> GetResultOrExceptionAsync<T>(Func<Task<TriedEx<T>>> callback)
+        public static async Task<TriedEx<T>> GetResultOrExceptionAsync<T>(
+            Func<Task<TriedEx<T>>> callback,
+            Func<Exception, Task>? errorCallback = null)
         {
             try
             {
@@ -113,6 +154,13 @@ namespace NexusLabs.Framework
             }
             catch (Exception ex)
             {
+                if (errorCallback != null)
+                {
+                    await errorCallback
+                        .Invoke(ex)
+                        .ConfigureAwait(false);
+                }
+
                 return ex;
             }
         }

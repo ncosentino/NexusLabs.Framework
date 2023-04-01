1ae74d87449f9b6418b042d7971d95a706890145
Nick Cosentino <n.b.cosentino@gmail.com>
2023-03-31 17:16:27 -0700

    AssumeAsFrozenDictionary -> Comparers

---
+++ NexusLabs.Collections.Generic.Tests/IReadOnlyDictionaryExtensionsTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+using Xunit;
+
+namespace NexusLabs.Collections.Generic.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public sealed class IReadOnlyDictionaryExtensionsTests
+    {
+        [Fact]
+        public void AssumeAsFrozenDictionary_CaseSensitiveStrings_ExpectedEntries()
+        {
+            Dictionary<string, string> dict = new()
+            {
+                ["Key1"] = "Val1",
+                ["Key2"] = "Val2",
+            };
+            var frozen = dict.AssumeAsFrozenDictionary();
+            Assert.Equal(2, frozen.Count);
+            Assert.Contains("Key1", frozen);
+            Assert.Contains("Key2", frozen);
+            Assert.DoesNotContain("KEY1", frozen);
+            Assert.DoesNotContain("KEY2", frozen);
+        }
+
+        [Fact]
+        public void AssumeAsFrozenDictionary_CaseInsensitiveStringsBeforeConversion_ExpectedEntries()
+        {
+            Dictionary<string, string> dict = new(StringComparer.OrdinalIgnoreCase)
+            {
+                ["Key1"] = "Val1",
+                ["Key2"] = "Val2",
+            };
+            var frozen = dict.AssumeAsFrozenDictionary();
+            Assert.Equal(2, frozen.Count);
+            Assert.Contains("Key1", frozen);
+            Assert.Contains("Key2", frozen);
+            Assert.DoesNotContain("KEY1", frozen);
+            Assert.DoesNotContain("KEY2", frozen);
+        }
+
+        [Fact]
+        public void AssumeAsFrozenDictionary_CaseInsensitiveStringsBeforeAndAfterConversion_ExpectedEntries()
+        {
+            Dictionary<string, string> dict = new(StringComparer.OrdinalIgnoreCase)
+            {
+                ["Key1"] = "Val1",
+                ["Key2"] = "Val2",
+            };
+            var frozen = dict.AssumeAsFrozenDictionary(StringComparer.OrdinalIgnoreCase);
+            Assert.Equal(2, frozen.Count);
+            Assert.Contains("Key1", frozen);
+            Assert.Contains("Key2", frozen);
+            Assert.Contains("KEY1", frozen);
+            Assert.Contains("KEY2", frozen);
+        }
+
+        [Fact]
+        public void AssumeAsFrozenDictionary_CaseInsensitiveStringsAfterConversion_ExpectedEntries()
+        {
+            Dictionary<string, string> dict = new()
+            {
+                ["Key1"] = "Val1",
+                ["Key2"] = "Val2",
+            };
+            var frozen = dict.AssumeAsFrozenDictionary(StringComparer.OrdinalIgnoreCase);
+            Assert.Equal(2, frozen.Count);
+            Assert.Contains("Key1", frozen);
+            Assert.Contains("Key2", frozen);
+            Assert.Contains("KEY1", frozen);
+            Assert.Contains("KEY2", frozen);
+        }
+    }
+}
--- NexusLabs.Collections.Generic/Extensions/IReadOnlyDictionaryExtensions.cs
+++ NexusLabs.Collections.Generic/Extensions/IReadOnlyDictionaryExtensions.cs
@@ -16,12 +16,14 @@ namespace System.Linq
         /// <typeparam name="TKey">The type of the key.</typeparam>
         /// <typeparam name="TValue">The type of the value.</typeparam>
         /// <param name="dictionary">The dictionary to use</param>
+        /// <param name="equalityComparer">The equality comparer to use. Optional.</param>
         /// <returns>
         /// A new <see cref="IFrozenDictionary{TKey, TValue}"/> reference or 
         /// if the current dictionary was frozen, the original reference.
         /// </returns>
         public static IFrozenDictionary<TKey, TValue> AssumeAsFrozenDictionary<TKey, TValue>(
-            this IReadOnlyDictionary<TKey, TValue> dictionary)
+            this IReadOnlyDictionary<TKey, TValue> dictionary,
+            IEqualityComparer<TKey> equalityComparer = default)
         {
             // since it's frozen we can directly return it already
             if (dictionary is IFrozenDictionary<TKey, TValue> frozenDictionary)
@@ -29,7 +31,9 @@ namespace System.Linq
                 return frozenDictionary;
             }
 
-            var frozen = new FrozenDictionary<TKey, TValue>(dictionary);
+            var frozen = new FrozenDictionary<TKey, TValue>(
+                dictionary,
+                equalityComparer);
             return frozen;
         }
     }
